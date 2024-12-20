0abca8284fb4bbce64d9f8e928b0584a3041797a
Tyler Dang (ThanhDanhhh) <danhdang270@gmail.com>
2024-12-20 10:24:05 +0700

    Config_MUI_Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-
-
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
 
 function App() {
-
   return (
     <>
       <div>TylerDangDev</div>
+      <Typography variant="Body2" color="text.secondary" >Hello World</Typography>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
