949bbe1a9f2e0076b08f3ec635a7b77cc0fecb0b
Tyler Dang (ThanhDanhhh) <danhdang270@gmail.com>
2024-12-21 23:01:33 +0700

    Dark_loght#1

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,31 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+  useColorScheme,
+} from '@mui/material/styles';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+
+      <hr />
       <div>TylerDangDev</div>
       <Typography variant="Body2" color="text.secondary" >Hello World</Typography>
       <Button variant="contained">Hello world</Button>
