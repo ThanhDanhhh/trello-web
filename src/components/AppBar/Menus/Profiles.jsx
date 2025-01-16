a0ca4ec0e61a5c02c1d82a1d2641c59ff35321a6
Tyler Dang (ThanhDanhhh) <danhdang270@gmail.com>
2025-01-16 10:55:56 +0700

    Refactor Theme - Color

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -33,7 +33,7 @@ function Profiles() {
           aria-haspopup="true"
           aria-expanded={open ? 'true' : undefined}
         >
-          <Avatar sx={{ width: 34, height: 34 }}
+          <Avatar sx={{ width: 36, height: 36 }}
             alt='TylerDangDev'
             src="https://avatars.githubusercontent.com/u/73952767?s=400&u=9ad1da9142e7d67edc92481ec83437fa3a4d65cb&v=4"
           />
