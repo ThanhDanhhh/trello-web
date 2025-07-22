f433972ee7893b43c2a4fc8d84b5d6baef41649e
Tyler Dang (ThanhDanhhh) <danhdang270@gmail.com>
2025-07-23 02:39:26 +0700

    Design Add Column & Card,UI/UX

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -8,7 +8,7 @@ function ListCards({ cards }) {
     <SortableContext items={cards?.map(c => c._id)} strategy={verticalListSortingStrategy}>
 
       <Box sx={{
-        m: '0 5px',
+        m: '0 5px 5px 5px',
         display: 'flex',
         flexDirection: 'column',
         gap: 1,
