b79ad62abcefcc7df447d1d6dac053b60d84c9f2
thanhdanh <danhtycos@gmail.com>
2025-07-29 03:26:19 +0700

    Implement drag and drop for cards with API#01

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -14,7 +14,7 @@ function ListColumns({ columns, createNewColumn, createNewCard }) {
 
   const [newColumnTitle, setNewColumTitle] = useState('')
 
-  const AddNewColumn = async () => {
+  const AddNewColumn = () => {
     if (!newColumnTitle) {
       toast.error('Please enter Column Title!')
       return
@@ -32,7 +32,7 @@ function ListColumns({ columns, createNewColumn, createNewCard }) {
      * component cha phía trên. (Đối với component con nằm càng sâu thì cảng khổ :D)
      * - Với việc sử dụng Redux như vậy thì code sẽ clean chuẩn chỉnh hơn rất nhiều.
      */
-    await createNewColumn(newColumnData)
+    createNewColumn(newColumnData)
 
     // Đóng trạng thái thêm Column mới & Clear Input
     toggleOpenNewColumnForm()
