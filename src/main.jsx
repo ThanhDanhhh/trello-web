3ad71db216515572b201e7eb7d8293bb41d7c14a
thanhdanh <danhtycos@gmail.com>
2025-08-01 01:54:06 +0700

    Implement full stack column & card deletion

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,24 @@ import theme from '~/theme'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+// Cấu hình MUI Dialog
+import { ConfirmProvider } from 'material-ui-confirm'
+
 // Caaus hinhf react-toastify
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <CssVarsProvider theme={theme}>
-    <CssBaseline />
-    <App />
-    <ToastContainer position='bottom-left' theme='colored' />
+    <ConfirmProvider defaultOptions={{
+      allowClose: false,
+      dialogProps: { maxWidth: 'xs' },
+      buttonOrder: ['confirm', 'cancel'],
+      confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
+      cancellationButtonProps: { color: 'inherit' }
+    }}>
+      <CssBaseline />
+      <App />
+      <ToastContainer position='bottom-left' theme='colored' />
+    </ConfirmProvider>
   </CssVarsProvider>
   // </React.StrictMode>,
 )
