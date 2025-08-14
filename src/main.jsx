40e434731760f9a7a718ab0bf3e5d87ced799fe3
thanhdanh <danhdang270@gmail.com>
2025-08-14 19:56:57 +0700

    feat(routing): Implement React Router DOM and basic pages (404)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import CssBaseline from '@mui/material/CssBaseline'
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '~/App.jsx'
-import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from '~/theme'
 
@@ -16,20 +16,26 @@ import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux'
 import { store } from '~/redux/store'
 
+// Cấu hình react-router-dom với BrowserRouter
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <CssVarsProvider theme={theme}>
-      <ConfirmProvider defaultOptions={{
-        allowClose: false,
-        dialogProps: { maxWidth: 'xs' },
-        buttonOrder: ['confirm', 'cancel'],
-        confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
-        cancellationButtonProps: { color: 'inherit' }
-      }}>
-        <CssBaseline />
-        <App />
-        <ToastContainer position='bottom-left' theme='colored' />
-      </ConfirmProvider>
-    </CssVarsProvider>
-  </Provider>
+  <BrowserRouter basename='/' future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+    <Provider store={store}>
+      <CssVarsProvider theme={theme}>
+        <ConfirmProvider defaultOptions={{
+          allowClose: false,
+          dialogProps: { maxWidth: 'xs' },
+          buttonOrder: ['confirm', 'cancel'],
+          confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
+          cancellationButtonProps: { color: 'inherit' }
+        }}>
+          <CssBaseline />
+          <App />
+          <ToastContainer position='bottom-left' theme='colored' />
+        </ConfirmProvider>
+      </CssVarsProvider>
+    </Provider>
+  </BrowserRouter>
+
 )
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 let apiRoot = ''
-console.log('import.meta.env', import.meta.env)
-console.log('process.env', process.env)
+// console.log('import.meta.env', import.meta.env)
+// console.log('process.env', process.env)
 
 if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
@@ -8,6 +8,6 @@ if (process.env.BUILD_MODE === 'dev') {
 if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-dh9c.onrender.com'
 }
-console.log('🚀 ~ apiRoot:', apiRoot)
+// console.log('🚀 ~ apiRoot:', apiRoot)
 
 export const API_ROOT = apiRoot
